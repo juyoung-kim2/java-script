91043fee2c76fb3f0c3cadde98f570c2b841e67e
김주영 <juyoung0145@gmail.com>
2025-01-23 21:59:01 +0900

    if문/삼항연산자

--- 250123_각도기.js
+++ 250123_각도기.js
@@ -3,33 +3,33 @@
 function solution(angle) {
 
 
-if (angle < 90){
-  return 1;
-} else if (angle == 90){
-  return 2;
-} else if (angle < 180) {
-  return 3;
-} else if (angle == 180) {
-  return 4;
-}
+  if (angle > 0 && angle < 90) {
+    return 1;
+  } else if (angle === 90) {
+    return 2;
+  } else if (angle > 90 && angle < 180) {
+    return 3;
+  } else if (angle === 180) {
+    return 4;
+  }
 }
 
 //조금 더 간단하게 수정
 function solution(angle) {
 
-  if (angle < 90){
+  if (angle < 90) {
     return 1;
-  } else if (angle == 90){
+  } else if (angle === 90) {
     return 2;
   } else if (angle < 180) {
     return 3;
-  } else if (angle == 180) {
+  } else if (angle === 180) {
     return 4;
   }
-  }
+}
 
 
 //삼항연산자
 function solution(angle) {
-    return angle < 90 ? 1 : angle === 90 ? 2 : angle < 180 ? 3 : 4;
+  return angle < 90 ? 1 : angle === 90 ? 2 : angle < 180 ? 3 : 4;
 }
