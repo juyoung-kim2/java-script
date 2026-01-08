ded71abda6a6dc8500c7603cebd7766c77dac904
김주영 <juyoung0145@gmail.com>
2026-01-08 21:06:21 +0900

    feat: day07 refactor validateInput to pure function and improve state management

---
+++ js-basics/day07-state-calculator/app.js
@@ -0,0 +1,65 @@
+//입력 검증 함수 : 가격 또는 수량이 빈값 / 0이하/ 숫자아니면 실패
+
+const calcBtn = document.getElementById('calcBtn');
+const result = document.getElementById('result');
+const priceEl = document.getElementById('price');
+const qtyEl = document.getElementById('qty');
+
+const state = {
+  price: 0,
+  qty: 0,
+  status: 'EMPTY',
+  message: ''
+}
+
+const STATUS = {
+  EMPTY: 'EMPTY',
+  NAN: 'NAN',
+  NON_POSITIVE: 'NON_POSITIVE',
+  OK: 'OK'
+}
+
+function validateInput(price, qty) {
+  if (!price || !qty) {
+    return "EMPTY"
+  } else if (isNaN(price) || isNaN(qty)) {
+    return "NAN"
+  } else if (price <= 0 || qty <= 0) {
+    return "NON_POSITIVE"
+  } else {
+    return "OK"
+  }
+};
+
+console.log(validateInput())
+function getMessageByStatus() {
+
+  switch (state.status) {
+    case STATUS.EMPTY: state.message = "가격과 수량을 입력해주세요.";
+    break;
+    case STATUS.NAN: state.message = "숫자만 입력해주세요.";
+    break;
+    case STATUS.NON_POSITIVE: state.message = "0보다 큰 값을 입력해주세요.";
+    break;
+    default:state.message = `총액은 ${(state.price * state.qty).toLocaleString()}원 입니다.`;
+  }
+  return state.message;
+}
+
+priceEl.addEventListener('input', handler);
+qtyEl.addEventListener('input', handler);
+calcBtn.addEventListener('click', handler);
+
+
+function handler() {
+  state.price = Number(priceEl.value);
+  state.qty = Number(qtyEl.value);
+  state.status = validateInput(state.price, state.qty);
+  getMessageByStatus();
+  render();
+}
+
+function render() {
+  result.textContent = state.message;
+  result.className = `result ${state.status === STATUS.OK ? 'result--ok':'result--error'}`
+}
