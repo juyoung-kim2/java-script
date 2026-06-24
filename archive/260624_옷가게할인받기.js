51cded86056cdf70cb81a1193c115e6d3fe479c0
juyoung-kim2 <juyoung0145@gmail.com>
2026-06-24 20:36:04 +0900

    260624_옷가게할인받기

---
+++ archive/260624_옷가게할인받기.js
@@ -0,0 +1,16 @@
+//머쓱이네 옷가게는 10만 원 이상 사면 5%, 30만 원 이상 사면 10%, 50만 원 이상 사면 20%를 할인해줍니다.
+//구매한 옷의 가격 price가 주어질 때, 지불해야 할 금액을 return 하도록 solution 함수를 완성해보세요.
+
+function solution(price) {
+  const answer =
+    100000 <= price < 300000
+      ? price * 0.95
+      : 300000 <= price < 500000
+        ? price * 0.9
+        : price >= 500000
+          ? price * 0.8
+          : price;
+  return answer;
+}
+
+console.log(solution(400000));
