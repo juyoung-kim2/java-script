197f9f9b68a9daa47b2f74d727c06a967088a870
juyoung-kim2 <juyoung0145@gmail.com>
2026-07-02 12:38:50 +0900

    260626_개미군단

--- archive/260626_진료순서정하기.js
+++ archive/260626_진료순서정하기.js
@@ -3,14 +3,6 @@
 // 응급도가 높은 순서대로 진료 순서를 정한 배열을 return하도록 solution 함수를 완성해주세요.
 
 function solution(emergency) {
-  const emergency = [30, 10, 23, 6, 100];
   const sort = [...emergency].sort((a, b) => b - a);
   return emergency.map((v, i) => sort.indexOf(v) + 1);
 }
-
-const emergency = [30, 10, 23, 6, 100];
-const sort = [...emergency].sort((a, b) => b - a);
-console.log(emergency);
-console.log(sort);
-const array = emergency.map((v, i) => sort.indexOf(v) + 1);
-console.log(array);
