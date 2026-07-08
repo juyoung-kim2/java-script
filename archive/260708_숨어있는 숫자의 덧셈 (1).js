239793abf56ef9955303fa0bfe73b92664ed5017
juyoung-kim2 <juyoung0145@gmail.com>
2026-07-08 13:03:38 +0900

    260708_숨어있는숫자의덧셈(1)

---
+++ archive/260708_숨어있는 숫자의 덧셈 (1).js
@@ -0,0 +1,7 @@
+//문자열 my_string이 매개변수로 주어집니다.
+//my_string안의 모든 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
+
+function solution(my_string) {
+  const NumArray = my_string.match(/\d/g).map(Number);
+  return NumArray.reduce((acc, crr) => acc + crr, 0);
+}
