c703553e438a385231b1fe61b1700518477bd8de
juyoung-kim2 <juyoung0145@gmail.com>
2026-07-16 17:13:37 +0900

    260716_영어가싫어요

---
+++ archive/260716_대문자와소문자.js
@@ -0,0 +1,14 @@
+//문자열 my_string이 매개변수로 주어질 때,
+//대문자는 소문자로 소문자는 대문자로 변환한 문자열을 return하도록 solution 함수를 완성해주세요.
+
+function solution(my_string) {
+  let result = "";
+  for (let char of my_string) {
+    if (char === char.toUpperCase()) {
+      result += char.toLowerCase();
+    } else {
+      result += char.toUpperCase();
+    }
+  }
+  return result;
+}
