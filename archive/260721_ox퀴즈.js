47894f4e5a83f9846f2d410656fab47a90523634
juyoung-kim2 <juyoung0145@gmail.com>
2026-07-21 15:32:48 +0900

    260721_ox퀴즈

---
+++ archive/260721_ox퀴즈.js
@@ -0,0 +1,10 @@
+//덧셈, 뺄셈 수식들이 'X [연산자] Y = Z' 형태로 들어있는 문자열 배열 quiz가 매개변수로 주어집니다.
+//수식이 옳다면 "O"를 틀리다면 "X"를 순서대로 담은 배열을 return하도록 solution 함수를 완성해주세요.
+
+function solution(quiz) {
+  return quiz.map((item) => {
+    const [X, op, Y, eq, Z] = item.split(" ");
+    const calc = op === "+" ? Number(X) + Number(Y) : Number(X) - Number(Y);
+    return calc === Number(Z) ? "O" : "X";
+  });
+}
